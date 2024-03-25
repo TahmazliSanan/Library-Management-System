b8336b6d4974b8a4751b6261b3f5c568e85eab0c
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-03-25 21:22:48 +0400

    Create DTO: AuthorDto

---
+++ src/main/java/org/pronet/payloads/AuthorDto.java
@@ -0,0 +1,24 @@
+package org.pronet.payloads;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class AuthorDto {
+    private Long id;
+    @NotEmpty(message = "Full name cannot be empty!")
+    private String fullName;
+    @NotEmpty(message = "Nationality cannot be empty!")
+    private String nationality;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date birthDate;
+    private Date createdAt;
+    private Date updatedAt;
+}
