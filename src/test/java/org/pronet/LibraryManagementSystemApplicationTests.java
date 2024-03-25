29fa626265c66a2ec8be048220548021f9f01fa4
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-03-25 20:20:06 +0400

    Create project

---
+++ src/test/java/org/pronet/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,11 @@
+package org.pronet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
