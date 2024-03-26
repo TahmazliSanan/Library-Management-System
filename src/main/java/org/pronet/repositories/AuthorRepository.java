ab285b3a6bdbb26eeadf319bac9d6dd2c866404a
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-03-26 12:01:29 +0400

    Create method: searchAuthors

--- src/main/java/org/pronet/repositories/AuthorRepository.java
+++ src/main/java/org/pronet/repositories/AuthorRepository.java
@@ -2,8 +2,13 @@ package org.pronet.repositories;
 
 import org.pronet.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    @Query(value = "SELECT * FROM authors WHERE full_name LIKE %:keyword%", nativeQuery = true)
+    List<Author> searchAuthors(String keyword);
 }
